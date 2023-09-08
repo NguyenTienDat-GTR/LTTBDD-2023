90d985a557b53a5738133097099420854728f4f0
NguyenTienDat-GTR <tiendat.107001@gmail.com>
2023-09-08 08:50:15 +0700

    Challenge 3 task 2

--- Challenge 3/js/Challenge3.js
+++ Challenge 3/js/Challenge3.js
@@ -13,3 +13,11 @@ var everageKoalaScore = CalculateEverage(koalaScore);
 console.log("everage KoalaScore: " + everageKoalaScore);
 
 // task 2
+function compare(everageDolphinScore, everageKoalaScore) {
+    if (everageDolphinScore > everageKoalaScore)
+        console.log("Dolphin is winner!");
+    else if (everageDolphinScore < everageKoalaScore)
+        console.log("Koala is winner!");
+    else console.log("the two teams drew");
+}
+compare(everageDolphinScore, everageKoalaScore)
