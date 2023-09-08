1d6806ac49a9f4c0bff78147a3b557baa4e496c3
NguyenTienDat-GTR <tiendat.107001@gmail.com>
2023-09-08 08:58:28 +0700

    Challenge 3 task 3&4

--- Challenge 3/js/Challenge3.js
+++ Challenge 3/js/Challenge3.js
@@ -21,3 +21,15 @@ function compare(everageDolphinScore, everageKoalaScore) {
     else console.log("the two teams drew");
 }
 compare(everageDolphinScore, everageKoalaScore)
+
+//task 3 and 4
+function checkMinmumScore(scores) {
+    return scores >= 100;
+}
+if (everageDolphinScore > everageKoalaScore && checkMinmumScore(everageDolphinScore))
+    console.log("Dolphin is winner!");
+else if (everageDolphinScore < everageKoalaScore && checkMinmumScore(everageKoalaScore))
+    console.log("Koala is winner!");
+else if (everageDolphinScore == everageKoalaScore && checkMinmumScore(everageDolphinScore) && checkMinmumScore(everageKoalaScore))
+    console.log("The two teams drew");
+else console.log("No one wins to the trophy ");
