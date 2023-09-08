24964ad7ad833d245d18f86d078db0a3fced4eeb
NguyenTienDat-GTR <tiendat.107001@gmail.com>
2023-09-08 08:02:36 +0700

    Test data 1 Challenge 1 lan 3

--- Challenge 1/js/challeng1.js
+++ Challenge 1/js/challeng1.js
@@ -1,10 +1,10 @@
 function BMI(mass, height) {
     return mass / (height * height)
 }
-const MarkMass = 78
-const MarkHeight = 1.69
-const JohnMass = 92
-const JonhHeight = 1.95
+const MarkMass = 95 //78
+const MarkHeight = 1.88 // 1.69
+const JohnMass = 85 //92
+const JonhHeight = 1.76 // 1.95
 
 var MarkBMI = BMI(MarkMass, MarkHeight)
 console.log("Mark's BMI: " + MarkBMI);
